2068c933d89b0d34849262d5ab0eb5c49544e665
Jonathan Hampton <hamptonjc221@gmail.com>
2021-03-08 00:17:43 -0500

    Updated layout

--- src/app/classifier/classifier.component.ts
+++ src/app/classifier/classifier.component.ts
@@ -17,6 +17,7 @@ export class ClassifierComponent implements OnInit {
   lesion_types = new Map<number, string>();
   type_pred: string;
   showSpinner: boolean = false;
+  wiki_link: string;
 
 
   constructor() { }
@@ -30,6 +31,7 @@ export class ClassifierComponent implements OnInit {
     this.lesion_types.set(4, 'Melanoma');
     this.lesion_types.set(5, 'Melanocytic Nevi');
     this.lesion_types.set(6, 'Vascular Skin Lesion');
+
     this.model = await tf.loadGraphModel('assets/tf_models/mobilenet_skin_lesion_v1/model.json');
     this.showSpinner = false;
   }
@@ -80,6 +82,7 @@ export class ClassifierComponent implements OnInit {
     let pred_tensor: tf.Tensor = await this.model.predict(tensor);
     this.predictions = pred_tensor.arraySync();
     this.onehot_2_str_pred(pred_tensor);
+    this.set_wiki_link(this.type_pred);
     this.showSpinner = false;
   }
 
@@ -88,4 +91,30 @@ export class ClassifierComponent implements OnInit {
     max_idx = max_idx.arraySync();
     this.type_pred = this.lesion_types.get(max_idx[0]);
   }
+
+  set_wiki_link(str_type) {
+    if (str_type == 'Actinic Keratosis') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Actinic_keratosis';
+    }
+    else if (str_type == 'Basal Cell Carcinoma') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Basal-cell_carcinoma';
+    }
+    else if (str_type == 'Benign Keratosis') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Seborrheic_keratosis';
+    }
+    else if (str_type == 'Dermatofibroma') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Dermatofibroma';
+    }
+    else if (str_type == 'Melanoma') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Melanoma';
+    }
+    else if (str_type == 'Melanocytic Nevi') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Melanocytic_nevus';
+    }
+    else if (str_type == 'Vascular Skin Lesion') {
+      this.wiki_link = 'https://en.wikipedia.org/wiki/Vascular_anomaly';
+    }
+    else throw new Error('Prediction type unknown for wiki link');
+  }
+
 }
